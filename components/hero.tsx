c36a1bbd96cf07b56ae327bd5932f5e35409c126
Prince Akpabio <princeakpabio90@gmail.com>
2023-11-29 00:10:16 +0100

    Fix: added all gsap code to the gsap context to test if the animations will play properly in prod

--- components/hero.tsx
+++ components/hero.tsx
@@ -53,100 +53,106 @@ export default function Hero() {
   }
 
   useLayoutEffect(() => {
-    gsap.registerPlugin(ScrollTrigger);
-    const scrollTimeline = gsap.timeline({
-      scrollTrigger: {
-        trigger: document.documentElement,
-        scrub: true,
-        start: "top",
-        end: "+90vh",
-      },
-    });
-
-    const normalTimeline = gsap.timeline({
-      defaults: {
-        duration: 0.6,
-        ease: "ease.in",
-        stagger: { repeat: 0, yoyo: true, yoyoEase: true },
-      },
-    });
-
-    const animatedScrollIconTimeline = gsap.timeline();
-
-    animatedScrollIconTimeline
-      .to(animatedScrollIcon.current, { opacity: 1 })
-      .from(ellipseOneItem.current, {
-        y: 0,
-        ease: "ease.out",
-        width: "18px",
-        height: "18px",
-        immediateRender: false,
-      })
-      .to(ellipseOneItem.current, {
-        y: "-25px",
-        width: "14px",
-        height: "18px",
-        repeat: -1,
-        yoyo: true,
-        ease: "sine.inOut",
+    let gsapCtx = gsap.context(() => {
+      gsap.registerPlugin(ScrollTrigger);
+      const scrollTimeline = gsap.timeline({
+        scrollTrigger: {
+          trigger: document.documentElement,
+          scrub: true,
+          start: "top",
+          end: "+90vh",
+        },
       });
 
-    normalTimeline
-      .from(heroIntroItem.current, { display: "none", immediateRender: false })
-      .to(heroIntroItem.current, { display: "block" })
-      .from(heroHireMeItem.current, { display: "none", immediateRender: false })
-      .to(heroHireMeItem.current, { display: "block", delay: 8 })
-      .from(worksMenuItem.current, {
-        opacity: 0,
-        y: "-100px",
-        immediateRender: false,
-      })
-      .to(worksMenuItem.current, { opacity: 1, y: 0 })
-      .from(contactMenuItem.current, {
-        opacity: 0,
-        y: "-100px",
-        immediateRender: false,
-      })
-      .to(contactMenuItem.current, { opacity: 1, y: 0 })
-      .from(resumeMenuItem.current, {
-        opacity: 0,
-        y: "-100px",
-        immediateRender: false,
-      })
-      .to(resumeMenuItem.current, { opacity: 1, y: 0 })
-      .from(firstNameItem.current, {
-        opacity: 0,
-        x: "200px",
-        delay: 0.7,
-        immediateRender: false,
-      })
-      .to(firstNameItem.current, { opacity: 1, x: 0, ease: "ease.in" })
-      .from(lastNameItem.current, {
-        opacity: 0,
-        x: "200px",
-        immediateRender: false,
-      })
-      .to(lastNameItem.current, { opacity: 1, x: 0, ease: "ease.in" })
-      .from(roleOneItem.current, {
-        opacity: 0,
-        x: "200px",
-        delay: 0.7,
-        immediateRender: false,
-      })
-      .to(roleOneItem.current, { opacity: 1, x: 0, ease: "ease.in" })
-      .from(roleTwoItem.current, {
-        opacity: 0,
-        x: "200px",
-        immediateRender: false,
-      })
-      .to(roleTwoItem.current, { opacity: 1, x: 0, ease: "ease.in" })
-      .add(animatedScrollIconTimeline);
+      const normalTimeline = gsap.timeline({
+        defaults: {
+          duration: 0.6,
+          ease: "ease.in",
+          stagger: { repeat: 0, yoyo: true, yoyoEase: true },
+        },
+      });
 
-    normalTimeline.play().delay(5.5);
+      const animatedScrollIconTimeline = gsap.timeline();
 
-    return () => {
-      gsap.killTweensOf(normalTimeline);
-    };
+      animatedScrollIconTimeline
+        .to(animatedScrollIcon.current, { opacity: 1 })
+        .from(ellipseOneItem.current, {
+          y: 0,
+          ease: "ease.out",
+          width: "18px",
+          height: "18px",
+          immediateRender: false,
+        })
+        .to(ellipseOneItem.current, {
+          y: "-25px",
+          width: "14px",
+          height: "18px",
+          repeat: -1,
+          yoyo: true,
+          ease: "sine.inOut",
+        });
+
+      normalTimeline
+        .from(heroIntroItem.current, {
+          display: "none",
+          immediateRender: false,
+        })
+        .to(heroIntroItem.current, { display: "block" })
+        .from(heroHireMeItem.current, {
+          display: "none",
+          immediateRender: false,
+        })
+        .to(heroHireMeItem.current, { display: "block", delay: 8 })
+        .from(worksMenuItem.current, {
+          opacity: 0,
+          y: "-100px",
+          immediateRender: false,
+        })
+        .to(worksMenuItem.current, { opacity: 1, y: 0 })
+        .from(contactMenuItem.current, {
+          opacity: 0,
+          y: "-100px",
+          immediateRender: false,
+        })
+        .to(contactMenuItem.current, { opacity: 1, y: 0 })
+        .from(resumeMenuItem.current, {
+          opacity: 0,
+          y: "-100px",
+          immediateRender: false,
+        })
+        .to(resumeMenuItem.current, { opacity: 1, y: 0 })
+        .from(firstNameItem.current, {
+          opacity: 0,
+          x: "200px",
+          delay: 0.7,
+          immediateRender: false,
+        })
+        .to(firstNameItem.current, { opacity: 1, x: 0, ease: "ease.in" })
+        .from(lastNameItem.current, {
+          opacity: 0,
+          x: "200px",
+          immediateRender: false,
+        })
+        .to(lastNameItem.current, { opacity: 1, x: 0, ease: "ease.in" })
+        .from(roleOneItem.current, {
+          opacity: 0,
+          x: "200px",
+          delay: 0.7,
+          immediateRender: false,
+        })
+        .to(roleOneItem.current, { opacity: 1, x: 0, ease: "ease.in" })
+        .from(roleTwoItem.current, {
+          opacity: 0,
+          x: "200px",
+          immediateRender: false,
+        })
+        .to(roleTwoItem.current, { opacity: 1, x: 0, ease: "ease.in" })
+        .add(animatedScrollIconTimeline);
+
+      normalTimeline.play().delay(5.5);
+    });
+
+    return () => gsapCtx.revert();
   }, []);
 
   return (
